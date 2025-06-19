0d7aeaa9b2bb75c8d8d6c7b3745795f07e129174
[Mahendra Kumar] <[zzsdrt354@gmail.com]>
2025-06-19 13:10:12 +0530

    summary of commit => 1]Loader info added in Js/Utilities/start_value.js. 2]capture environment removed in index.html.

--- Js/Utilities/start_value.js
+++ Js/Utilities/start_value.js
@@ -1,6 +1,7 @@
 import { notification } from "../../Notification.js";
 import { start_api_gate_way, start_backend } from "./start_backend.js";
 export async function start_value() {
+    Loader_info("Backend booting up...");
     try {
         if (!localStorage.getItem("key")) {
             return;
@@ -14,10 +15,12 @@ export async function start_value() {
         })
         if (!response.ok) {
             const data = await response.json();
+            Loader_info_remove();
             notification("#F44336", "white", data.message);
         }
         if (response.ok) {
             const data = await response.json();
+            Loader_info_remove();
             if (data.value == 1) {
                 start_api_gate_way();
                 setTimeout(() => {
@@ -28,4 +31,14 @@ export async function start_value() {
     } catch (error) {
         console.error(error);
     }
+}
+let loader_h4;
+function Loader_info(text) {
+    loader_h4 = document.createElement("h4");
+    loader_h4.innerText = text;
+    document.body.appendChild(loader_h4);
+}
+
+function Loader_info_remove() {
+    loader_h4.remove();
 }
